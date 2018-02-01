f8f2d856e2360a01e4570f626fef808c4e90a634
FilipApostolovic <35694411+FilipApostolovic@users.noreply.github.com>
2018-02-01 16:29:08 +0100

    Sitne promene

--- NodelLabVezba2/zadatak3.js
+++ NodelLabVezba2/zadatak3.js
@@ -1,1 +1,3 @@
-﻿
+﻿for (var i = 0; i < 7; i++) {
+    console.log(Math.floor(Math.random() * 10));
+}
--- NodelLabVezba2/zadatak4.js
+++ NodelLabVezba2/zadatak4.js
@@ -1,1 +1,4 @@
-﻿
+﻿var pravougaonik = require('./pravougaonik');
+
+pravougaonik.obim(100, 500);
+pravougaonik.povrsina(100, 500);
